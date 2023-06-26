1433bdc397df156d51b91ab3bfc006e25da8cf65
Manon <manonlefkos@gmail.com>
2023-06-26 21:47:06 +0200

    add gameBoard Array creation and test

---
+++ src/gameBoard.js
@@ -0,0 +1,21 @@
+export default class GameBoard {
+  constructor() {
+    this.array = this.createGameArray();
+  }
+
+  createGameArray() {
+    const columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    const gameArray = [];
+
+    for (let i = 0; i < rows.length; i++) {
+      columns.forEach((index) => {
+        const cell = {
+          data: rows[i] + index
+        };
+        gameArray.push(cell)
+      });
+    }
+    return gameArray
+  }
+}
