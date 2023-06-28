387c7e8fc08f72947ef8478783e826a4935e11da
Manon <manonlefkos@gmail.com>
2023-06-28 17:20:40 +0200

    add player attack function

--- src/player.js
+++ src/player.js
@@ -4,4 +4,8 @@ export default class Player {
   constructor() {
     this.board = new GameBoard
   }
+
+  attackEnemy(enemy, coord) {
+    enemy.board.receiveAttack(coord)
+  }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -3,3 +3,7 @@ import Player from "./player";
 const human = new Player
 const ai = new Player
 
+test("1.1. expect human to attack computer a1 coordinate and update the hit", () => {
+  human.attackEnemy(ai, "a1")
+  expect(ai.board.array[0].hit).toBe(true)
+})
